ba441c44eb49af4096349fa1d3be6ca68c8e0a8f
Artem Andriushchenko <link.by.artem@gmail.com>
2024-09-04 03:32:14 +0300

    Added parameters for connecting to DB to config.py

--- config.py
+++ config.py
@@ -11,6 +11,14 @@ from dotenv import load_dotenv
 # Завантаження змінних середовища з файлу .env
 load_dotenv()
 
+# API Configurations
 API_BASE_URL = os.getenv("API_BASE_URL")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+# Database Configurations
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
