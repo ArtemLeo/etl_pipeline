34366aabbd6b3f5297b174c1b9006d3222ca64dc
Artem Andriushchenko <link.by.artem@gmail.com>
2024-09-04 04:12:13 +0300

    Updated main.py to create a table

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Для запуску ETL процесу
 
 
-from etl_pipeline import connect_to_db
+from etl_pipeline import connect_to_db, create_tables
 
 if __name__ == "__main__":
     db_connection = connect_to_db()
     if db_connection:
+        create_tables(db_connection)
         db_connection.close()
